2b593e7ce6ba94acadbdfe9b554b8e9e320b3a43
a.vinogradov <a.vinogradov@bpcbt.com>
2024-03-02 18:31:38 +0300

    logic

--- src/components/pages/ViewBookPage/ViewControl/ViewControl.tsx
+++ src/components/pages/ViewBookPage/ViewControl/ViewControl.tsx
@@ -1,12 +1,6 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {
-  ArrowLeftIcon,
-  Cog6ToothIcon,
-  PaintBrushIcon,
-  WrenchIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/solid';
+import { XMarkIcon } from '@heroicons/react/24/solid';
 import cn from 'clsx';
 import WidthControl from '@/components/common/WidthControl/WidthControl.tsx';
 import routes from '@/constants/routes.ts';
@@ -18,6 +12,8 @@ interface Props extends React.ComponentPropsWithoutRef<'div'> {}
 function ViewControl({ className, ...props }: Props) {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const hideMenuTimeoutIdRef = useRef<ReturnType<typeof setTimeout>>();
 
   const handleClickBack = () => {
     navigate(routes.selectBook);
@@ -30,31 +26,49 @@ function ViewControl({ className, ...props }: Props) {
     [dispatch],
   );
 
+  const handleMouseOverTopArea = () => {
+    setIsMenuVisible(true);
+    clearTimeout(hideMenuTimeoutIdRef.current);
+  };
+
+  const handleMouseLeaveTopArea = () => {
+    hideMenuTimeoutIdRef.current = setTimeout(() => {
+      setIsMenuVisible(false);
+    }, 1000);
+  };
+
   return (
     <div
-      className={cn(
-        'fixed flex justify-between top-0 left-0 w-full bg-white shadow-xl text-slate-500 items-center',
-        className,
-      )}
-      {...props}
+      className="h-[200px] fixed top-0 left-0 w-full"
+      onMouseOver={handleMouseOverTopArea}
+      onMouseLeave={handleMouseLeaveTopArea}
     >
-      <div className="pl-4">
-        <div>Author</div>
-        <div>Title</div>
-      </div>
-      <div className="flex gap-8 items-center px-2">
-        <div className="flex gap-2">
-          <div className="text-sm">
-            Content width:
-          </div>
-          <div className="w-[200px]">
-            <WidthControl onChange={handleChangeWidth}></WidthControl>
-          </div>
+      <div
+        className={cn(
+          'absolute top-0 w-full flex justify-between bg-white shadow-lg text-slate-500 items-center h-12 -translate-y-full transition',
+          className,
+          { ['-translate-y-0']: isMenuVisible },
+        )}
+        {...props}
+      >
+        <div className="pl-4">
+          <div className="-mb-1">Author</div>
+          <div>Title</div>
         </div>
+        <div className="flex items-center px-2 min-h-full">
+          <div className="flex gap-2">
+            <div className="text-sm">Content width:</div>
+            <div className="w-[200px] px-2">
+              <WidthControl onChange={handleChangeWidth}></WidthControl>
+            </div>
+          </div>
 
-        <button type="button" onClick={handleClickBack} className="size-8 hover:text-red-500">
-          <XMarkIcon />
-        </button>
+          <div className="h-10 w-[1px] bg-slate-300 ml-4 mr-2" />
+
+          <button type="button" onClick={handleClickBack} className="size-8 hover:text-red-500">
+            <XMarkIcon />
+          </button>
+        </div>
       </div>
     </div>
   );
